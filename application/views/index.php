9bd1155d34f420e7429b13c67eb5b5a2f0152902
swiftbf1742 <amrutayenare81@gmail.com>
2016-10-04 13:17:00 +0530

    file changes

--- application/views/index.php
+++ application/views/index.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Rejuvenesse breathe new life</title>
+<title>Rejuvenesse breathe new lifes</title>
 <!--mobile apps-->
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
